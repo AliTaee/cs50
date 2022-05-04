6f77a85d85c032d568c919b9868ba08948e7d78d
AliTaee <ali.taee.72@gmail.com>
2022-05-04 11:40:37 +0430

    refactor week4 copy file from js to ts

--- week4/copy.ts
+++ week4/copy.ts
@@ -14,13 +14,20 @@ console.log({ person, person2 })
 */
 
 // Correct way ✅
-const person = {
+interface Person {
+  name: string
+  family: string
+  tell: number
+  address: string
+}
+
+const person: Person = {
   name: 'Ali',
   family: 'Taee',
   tell: 9361702478,
   address: 'Tehran, ejare dar',
 }
 
-let person2 = { ...person, name: 'Sadaf' }
+let person2 = { ...person, name: 'Ted' }
 
 console.log({ person, person2 })
